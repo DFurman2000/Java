37218ca7746b4be2240fadfff6e4b35065c03618
U1958141 <u1958141@unimail.hud.ac.uk>
2020-01-21 09:39:41 +0000

    Term 2 Week 1

---
+++ src/week1/tempScanner.java
@@ -0,0 +1,33 @@
+// This programme will read in 5 temperatures from the weather station
+// Each reading has a value of the temperature and is represented as an integer followed by a single character (e.g. 23C)
+// You will need a scanner to read in data, a loop to collect 5 readings, an array list to store the integer values
+// and a process of printing the contents of the array list (min, max, average and count.)
+
+package week1;
+
+import java.util.Scanner;
+
+public class tempScanner {
+    public static void intro (String name) {
+        System.out.println("Welcome to our weather station. Please enter 5 readings in celsius.");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Next reading: ");
+        String s = in.nextLine();
+        try {
+            if (s.endsWith("C")) {
+                int temp = Integer.parseInt(s.substring(0, s.length() - 1));
+                System.out.println("Temperature = " + temp);
+            } else {
+                throw new NumberFormatException();
+            }
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Wrong Format. Please add a C next to the temperature.");
+        }
+        }
+}
